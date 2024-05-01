2b7fcc9dd3a59fe03ba04544cbe3c9a10bbc1bad
daniaedu <rocketbookdania@gmail.com>
2024-05-01 18:08:46 -0500

    Add window event listener that loads grid on start

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const gridContainer = document.querySelector('.gridContainer');
 
-// Creates a grid based on number (if number = 8, then creates 8x8 grid)
-function gridCreation(number) {
-    for (let row = 0; row < number; row++) {
-        for (let col = 0; col < number; col++) {
-            const flexBasis = 100 / number;
-            const gridCell = document.createElement("div")
-            gridCell.classList.add('gridCell');
-            gridCell.style.flex = `0 0 ${flexBasis}%`;
-            gridContainer.appendChild(gridCell);
+// On load, it creates a grid with a default number of 16
+window.addEventListener('load', gridCreation);
+
+// Creates game grid
+function gridCreation(event) {
+    if (event.type === 'load') {
+        const number = 16;
+        for (let row = 0; row < number; row++) {
+            for (let col = 0; col < number; col++) {
+                const flexBasis = 100 / number;
+                const gridCell = document.createElement("div")
+                gridCell.classList.add('gridCell');
+                gridCell.style.flex = `0 0 ${flexBasis}%`;
+                gridContainer.appendChild(gridCell);
+            }
         }
     }
 }
